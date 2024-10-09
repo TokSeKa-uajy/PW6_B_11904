aab7c22852f5416c7fb2834e16a75d55463a680b
TokSeKa-uajy <220711904@students.uajy.ac.id>
2024-10-09 22:00:06 +0700

    PW6_B_11904
    GUIDED

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+
+Route::resource('/product',  App\Http\Controllers\ProductController::class);
